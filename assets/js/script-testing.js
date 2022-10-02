bcf91befec947ff5fe9693ca52e1ec202047f3de
Redbutcher <paul34913@gmail.com>
2022-10-02 21:56:46 +0100

    script-testing update

--- assets/js/script-testing.js
+++ assets/js/script-testing.js
@@ -11,28 +11,18 @@ function splitString(str) {
         if (str[i+1] === undefined) {
 
             str1 = str[i] + '_';
-            
+                       
         } else {
 
             str1 = str[i] + str[i+1]
             console.log(str1);
-
         }
-
-        
-
 
         arr.push(str1);
         console.log(arr);
-
     }
 
     return arr;
-
 }
-
-
-
-
 
 splitString('123 4');
